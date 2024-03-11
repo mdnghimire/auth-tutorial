cfa07f0b39ed65139654caba2a72718e45b1a73b
mdnghimire <madan.ghimire@xrdig.com>
2024-03-11 16:14:23 +0545

    added border on home page card

--- app/page.tsx
+++ app/page.tsx
@@ -12,14 +12,14 @@ const font = Poppins({
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-sky-400 via-blue-600 to-blue-800">
-      <div className="space-y-6 text-center">
+      <div className="space-y-6 text-center  text-white border-2 border-white rounded-lg shadow-lg p-10">
         <h1
           className={cn(
             "text-6xl font-semibold text-white drop-shadow-md",
             font.className
           )}
         >
-          🔐 Auth
+          🔐 Auth.js
         </h1>
         <p className="text-white text-lg">A simple authentication service</p>
         <LoginButton mode="modal" asChild>
